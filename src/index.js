bfbc0aacb028ab3ac45f533f2ca3a036ddfcac81
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-06-07 23:27:51 +0300

    FT-07&FT-02 refactored

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 //Здесь определение всех глобал переменніх проекта
 //Константі, ключи
-const HOME_PAGE = "Home page",
-    MYLIBRARY_PAGE = "My library page";
+const HOME_PAGE = 'Home page',
+  MYLIBRARY_PAGE = 'My library page';
 //Изменяеміе
 const global = {
-    currentUser: "",
-    currentPage: HOME_PAGE,
-    currentGalleryPage: 1,
-}
+  currentUser: '',
+  currentPage: HOME_PAGE,
+  currentGalleryPage: 1,
+};
 //Віше определение всех глобал переменніх проекта
 
 // import './js/go';
 import './js/modal';
-import './js/homepage-render';
+import './js/tranding';
 
 import './js/goToMyLibrary';
---
+++ src/js/clearGallery.js
@@ -0,0 +1,5 @@
+import { refs } from './refs';
+
+export function clearGalleryContainer() {
+  refs.galleryContainer.innerHTML = '';
+}
