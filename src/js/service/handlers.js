2d0425118a545368b38f325c3fb61ebd1baa5ab3
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-06-14 21:19:03 +0200

    modal/refactoring
    Я закінчив рефакторінг. Файл modal.js видалив. Перевір свою функцію вона у файлі add-muvie-to-database папка service.

--- src/js/service/handlers.js
+++ src/js/service/handlers.js
@@ -2,6 +2,8 @@ import { loadTrandingPage } from '../js-partials/tranding-gallery.js';
 import { paginationList, inputForm } from '../utils/references';
 import { onPaginationBtnClick } from '../js-partials/pagination-btn-click';
 import { searchSubmit } from '../js-partials/search-submit';
+import { openModal } from '../js-partials/open-modal';
+import { openModalFilm } from '../utils/references';
 
 addEventListener('DOMContentLoaded', () => {
   loadTrandingPage();
@@ -10,3 +12,5 @@ addEventListener('DOMContentLoaded', () => {
 paginationList.addEventListener('click', onPaginationBtnClick);
 
 inputForm.addEventListener('submit', searchSubmit);
+
+openModalFilm.addEventListener('click', openModal);
