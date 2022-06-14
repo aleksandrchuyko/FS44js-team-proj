235240638d9cf3856aab782dbd9eba013bda2d86
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-06-15 00:40:06 +0300

    3

--- src/js/My-library-btn.js
+++ src/js/My-library-btn.js
@@ -3,10 +3,8 @@ import { refs } from './refs';
 import { clearPaginationList } from './clear-pagination';
 import clearGalleryContainer from './clear-gallery';
 import changeMoviesArray from './page-render';
-import { logIn, userId } from "./logIn";
-import { getUserDataAllWatched, getUserDataAllQueue } from "./getFromDataBase";
-
-
+import { logIn, userId } from './logIn';
+import { getUserDataAllWatched, getUserDataAllQueue } from './getFromDataBase';
 
 refs.queueBtn.addEventListener('click', clickButQueue);
 refs.watchedBtn.addEventListener('click', clickButWatched);
@@ -25,16 +23,15 @@ refs.navLibrary.addEventListener('click', clickButLibrary);
 // }
 
 function clickButLibrary(e) {
-
-    e.preventDefault();
-    refs.paginationList?.classList.add('is-hidden');
-    refs.navLibrary.classList.add('current');
-    refs.navHome.classList.remove('current');
-    refs.inputForm?.classList.add('visually-hidden');
-    refs.libraryBtns.classList.remove('visually-hidden');
-    moviesRender('queue')
-    refs.queueBtn.classList.add('active');
-    refs.watchedBtn.classList.remove('active');
+  e.preventDefault();
+  refs.paginationList?.classList.add('is-hidden');
+  refs.navLibrary.classList.add('current');
+  refs.navHome.classList.remove('current');
+  refs.inputForm?.classList.add('visually-hidden');
+  refs.libraryBtns.classList.remove('visually-hidden');
+  moviesRender('queue');
+  refs.queueBtn.classList.add('active');
+  refs.watchedBtn.classList.remove('active');
 }
 
 function clickButQueue() {
@@ -50,38 +47,37 @@ function clickButWatched() {
 // Render Library JS//////
 
 const container = document.getElementById('pagination');
-async function moviesRender(type) {
-    await clearGalleryContainer();
-    await clearPaginationList();
-    container?.classList.add('is-hidden');
-    try {
-        if (!global.userId) {
-            await logIn()
-        }
+// async function moviesRender(type) {
+//     await clearGalleryContainer();
+//     await clearPaginationList();
+//     container?.classList.add('is-hidden');
+//     try {
+//         if (!global.userId) {
+//             await logIn()
+//         }
 
-
-        switch (type) {
-            case 'watched':
-                getUserDataAllWatched(global.userId).then(data => {
-                    // global.watchedCache = Object.values(data);
-                    console.log('Массив watched из firebase:', data);
-                    await changeMoviesArray(data)
-                });
-                refs.galleryContainer.setAttribute('data-set', 'watched');
-                break;
-            case 'queue':
-                getUserDataAllQueue(global.userId).then(data => {
-                    // global.queueCache = Object.values(data);
-                    console.log('Массив queue из firebase:', data);
-                    await changeMoviesArray(data)
-                });
-                console.log(refs.galleryContainer)
-                refs.galleryContainer.setAttribute('data-set', 'queue');
-        }
-    } catch (e) {
-        console.error(e)
-    }
-}
+//         switch (type) {
+//             case 'watched':
+//                 getUserDataAllWatched(global.userId).then(data => {
+//                     // global.watchedCache = Object.values(data);
+//                     console.log('Массив watched из firebase:', data);
+//                     await changeMoviesArray(data)
+//                 });
+//                 refs.galleryContainer.setAttribute('data-set', 'watched');
+//                 break;
+//             case 'queue':
+//                 getUserDataAllQueue(global.userId).then(data => {
+//                     // global.queueCache = Object.values(data);
+//                     console.log('Массив queue из firebase:', data);
+//                     await changeMoviesArray(data)
+//                 });
+//                 console.log(refs.galleryContainer)
+//                 refs.galleryContainer.setAttribute('data-set', 'queue');
+//         }
+//     } catch (e) {
+//         console.error(e)
+//     }
+// }
 
 refs.navLibrary.addEventListener('click', () => {
   if (refs.watchedBtn.classList.contains('library__current')) {
@@ -93,4 +89,3 @@ refs.navLibrary.addEventListener('click', () => {
 
 refs.watchedBtn.addEventListener('click', () => moviesRender('watched'));
 refs.queueBtn.addEventListener('click', () => moviesRender('queue'));
-
