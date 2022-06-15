c9508dca6f11438ade28eaf3284719f8ad978fda
aleksandrchuyko <chao.mess@gmail.com>
2022-06-15 14:27:56 +0300

    Update logIn.js

--- src/js/logIn.js
+++ src/js/logIn.js
@@ -12,20 +12,8 @@ let userId = '';
 
 function logIn() {
   return signInWithPopup(auth, provider).then(result => {
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // userId = user.providerData[0].uid;
-    console.log(userId);
-    // console.log(auth);
       return user.email;
-
-
     }).catch((error) => {
-
       // Handle Errors here.
       const errorMessage = error.message;
       return errorMessage;
