56b7227339bee322835fe1e4efc870cee8b9a18a
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-06-16 17:49:06 +0200

    modal_tm-final-v.2

---
+++ src/js/js-partials/modal_tm.js
@@ -0,0 +1,31 @@
+import { openModalTm, closeModalTm, backdropTm } from '../utils/references';
+
+openModalTm.addEventListener('click', onOpenModal);
+closeModalTm.addEventListener('click', onCloseModal);
+backdropTm.addEventListener('click', onBackdropClick);
+
+function onOpenModal(e) {
+  e.preventDefault()
+  window.addEventListener('keydown', onEscKeyPress);
+  document.body.classList.add('show-modal');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  document.body.classList.remove('show-modal');
+}
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
+}
