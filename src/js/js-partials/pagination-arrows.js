a800e4c21a21d801922aa2fa6b45e82684846eb1
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-06-17 14:24:49 +0300

    2

--- src/js/js-partials/pagination-arrows.js
+++ src/js/js-partials/pagination-arrows.js
@@ -1,20 +1,21 @@
 import { transmitToRenderNextPage } from '../service/transmit-to-render-next-page';
 
 let currentPage;
+let page;
 
 export async function onLeftButtonClick(e) {
-    e.preventDefault();
-    currentPage = document.querySelector('.pagination__active');
-    console.log(currentPage);
-    page = Number(currentPage.textContent) - 1;
-    console.log(page);
-    await transmitToRenderNextPage(page);
+  e.preventDefault();
+  currentPage = document.querySelector('.pagination__active');
+  console.log(currentPage);
+  page = Number(currentPage.textContent) - 1;
+  console.log(page);
+  await transmitToRenderNextPage(page);
 }
 
 export async function onRightButtonClick(e) {
-    e.preventDefault();
-    currentPage = document.querySelector('.pagination__active');
-    page = Number(currentPage.textContent) + 1;
-    console.log(page);
-    await transmitToRenderNextPage(page);
-  }
+  e.preventDefault();
+  currentPage = document.querySelector('.pagination__active');
+  page = Number(currentPage.textContent) + 1;
+  console.log(page);
+  await transmitToRenderNextPage(page);
+}
